9537361b0f960fb5bda243c03945f9c2ef7f7e60
lei.tian <whereismoney@qq.com>
2020-09-15 11:25:27 +0800

    feat: 增加SsoClient调用支持

--- src/functions.php
+++ src/functions.php
@@ -53,3 +53,48 @@ function RestServiceGet($path, $params, $headers = [])
     }
     return \RestAPI\RestServiceClient::get($path, $params, $headers);
 }
+
+function SsoClientInitialize($accessToken, $headers = [])
+{
+    \RestAPI\Client::initialize(
+        \RestAPI\Helper::getEnv('restapi.sys_id'),
+        \RestAPI\Helper::getEnv('restapi.secret_key'),
+        $accessToken
+    );
+    \RestAPI\Client::useRegion(\RestAPI\Helper::getEnv('restapi.region'));
+    // timeout
+    if (isset($headers['timeout']) && !empty($headers['timeout'])) {
+        \RestAPI\Client::setTimeout($headers['timeout']);
+        unset($headers['timeout']);
+    }
+    // server_url
+    if ($serverUrl = \RestAPI\Helper::getEnv('restapi.server_url')) {
+        empty($serverUrl) || \RestAPI\Client::setServerUrl($serverUrl);
+    }
+}
+
+function SsoClientGet($accessToken, $path, $params, $headers = [])
+{
+    SsoClientInitialize($accessToken, $headers);
+    return \RestAPI\Client::get($path, $params, $headers);
+}
+
+function SsoClientPost($accessToken, $path, $params, $headers = [])
+{
+    SsoClientInitialize($accessToken, $headers);
+    return \RestAPI\Client::post($path, $params, $headers);
+}
+
+function SsoClientPut($accessToken, $path, $params, $headers = [])
+{
+    SsoClientInitialize($accessToken, $headers);
+    return \RestAPI\Client::put($path, $params, $headers);
+}
+
+function SsoClientDelete($accessToken, $path, $params, $headers = [])
+{
+    SsoClientInitialize($accessToken, $headers);
+    return \RestAPI\Client::delete($path, $params, $headers);
+}
+
+
